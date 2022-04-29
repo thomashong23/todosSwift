856be21a64747ff0b099377e3850a789a26000df
Thomas Hong <thomas_hong23@milton.edu>
2022-04-29 10:31:53 -0400

    Initial Commit

---
+++ practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  practice
+//
+//  Created by Tommy Hong on 4/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ practice/practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceApp.swift
+//  practice
+//
+//  Created by Tommy Hong on 4/29/22.
+//
+
+import SwiftUI
+
+@main
+struct practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
